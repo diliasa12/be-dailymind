632739ea1f13205af9187e2174995ff1d29f0313
dili <muhammaddilia99@gamil.com>
2026-04-02 14:04:11 +0700

    feat:auth selesai

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,2 +1,3 @@
 import { drizzle } from "drizzle-orm/node-postgres";
-export const db = drizzle(process.env.DATABASE_URL!);
+import * as schema from "@/schema/auth-schema";
+export const db = drizzle(process.env.DATABASE_URL!, { schema });
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,41 @@
 import { Hono } from "hono";
+import { auth } from "@/lib/auth";
 import { drizzle } from "drizzle-orm/node-postgres";
-const db = drizzle(process.env.DATABASE_URL!);
-const app = new Hono();
 
+import { cors } from "hono/cors";
+
+const app = new Hono<{
+  Variables: {
+    user: typeof auth.$Infer.Session.user | null;
+    session: typeof auth.$Infer.Session.session | null;
+  };
+}>();
+
+app.use(
+  "*",
+  cors({
+    origin: "http://localhost:5173", // Tambahkan 'null' jika buka file langsung
+    allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowHeaders: ["Content-Type", "Authorization"],
+    exposeHeaders: ["Content-Length"],
+    credentials: true,
+  }),
+);
+app.use("*", async (c, next) => {
+  const session = await auth.api.getSession({ headers: c.req.raw.headers });
+  if (!session) {
+    c.set("user", null);
+    c.set("session", null);
+    await next();
+    return;
+  }
+  c.set("user", session.user);
+  c.set("session", session.session);
+  await next();
+});
+app.on(["POST", "GET"], "/api/auth/*", (c) => {
+  return auth.handler(c.req.raw);
+});
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
